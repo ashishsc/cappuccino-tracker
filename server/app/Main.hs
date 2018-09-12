119bca61c82f1f52bbde4260033244e53df8bad8
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2018-09-12 00:29:36 -0400

    Enable creating new purchases
    Also, use hindent in the same change because I am a terrible person

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,42 +1,67 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 
-module Main (main) where
+module Main
+  ( main
+  ) where
 
-import System.Environment
 import GHC.Generics
+import System.Environment
 
-import qualified Web.Scotty
-import qualified Data.Text.Lazy as LazyText
-import qualified Data.Text as Text
 import Control.Monad.Trans (liftIO)
-import Database.PostgreSQL.Simple
 import Data.Aeson
+import Data.Int (Int64)
+import qualified Data.Text as Text
+import qualified Data.Text.Lazy as LazyText
+import Database.PostgreSQL.Simple
+import Network.HTTP.Types.Status (status200)
+import qualified Web.Scotty
 
-data Purchase = Purchase { id :: Int
-                         , description :: String
-                         , cents :: Int
-                         } deriving (Generic, ToJSON, FromJSON)
+data Purchase = Purchase
+  { id :: Int
+  , description :: String
+  , cents :: Int
+  } deriving (Generic, ToJSON, FromJSON)
 
-
+data NewPurchase = NewPurchase
+  { description :: String
+  , cents :: Int
+  } deriving (Generic, ToJSON, FromJSON)
 
 main :: IO ()
 main = do
   connection <- connect defaultConnectInfo
   Web.Scotty.scotty 6969 $ do
     Web.Scotty.get "/all" $ getTotalAction connection
-
-
+    Web.Scotty.post "/purchase" $ addPurchaseAction connection
 
 getTotalAction :: Connection -> Web.Scotty.ActionM ()
 getTotalAction connection = do
-    liftIO $ putStrLn "fetching"
-    purchaseTuples <- liftIO $ getTotalFromDb connection
-
+  liftIO $ putStrLn "fetching"
+  purchaseTuples <- liftIO $ getTotalFromDb connection
     -- some time that needs to be json
-    Web.Scotty.json $ fmap (\(id, description, cents) -> Purchase {Main.id = id, description = description, cents = cents}) purchaseTuples
+  Web.Scotty.json $
+    fmap
+      (\(id, description, cents) -> Purchase id description cents)
+      purchaseTuples
+
+addPurchaseAction :: Connection -> Web.Scotty.ActionM ()
+addPurchaseAction connection = do
+  liftIO $ putStrLn "adding purchase"
+  newPurchase <- Web.Scotty.jsonData :: Web.Scotty.ActionM NewPurchase
+  rowsAffected <- liftIO $ addPurchaseToDb connection newPurchase
+  liftIO $ putStrLn (show rowsAffected)
+  Web.Scotty.status status200
 
 getTotalFromDb :: Connection -> IO [(Int, String, Int)]
 getTotalFromDb connection = do
   xs <- query_ connection "select * from cap.purchases"
   return xs
+
+addPurchaseToDb :: Connection -> NewPurchase -> IO Int64
+addPurchaseToDb connection NewPurchase {description = d, cents = c} = do
+  execute
+    connection
+    "INSERT INTO cap.purchases (description, cents) VALUES (?, ?)"
+    (d, c)
